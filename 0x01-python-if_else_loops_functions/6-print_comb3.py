645e4f31cfda729084b36218937582dbd859aebf
hellerdejanuar <hellerdejanuar@gmail.com>
2022-04-26 13:46:40 -0700

    8 added

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-first = False
+notfirst = False
 for i in range(10):
     for j in range(i+1, 10):
-        if first:
+        if notfirst:
             print(end=', ')
         else:
-            first = True
+            notfirst = True
         print(f"{i}{j}", end='')
 print()
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+def uppercase(str):
+
+# iterate through letters in string
+    for letter in str:
+# if lowercase 
+        ascii_ch = ord(letter)
+        if ascii_ch >= 97 and ascii_ch <= 122:
+            letter = chr(ascii_ch - 32)
+        print(letter, end='')
+
+    print()
