0d6cef5550b96e0800d94ac09146159c1b198648
hellerdejanuar <hellerdejanuar@gmail.com>
2022-04-26 14:36:27 -0700

    12 added

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+def fizzbuzz():
+    for i in range(1,100):
+        flag = False
+        if i % 3 == 0:
+            print("Fizz", end='')
+            flag = True
+        if i % 5 == 0:
+            print("Buzz", end='')
+            flag = True
+        if not flag:
+            print(i, end='')
+        print(end=' ')
