6c17d59c8467575cecbb0f5d0a3536cb67d19a18
hellerdejanuar <hellerdejanuar@gmail.com>
2022-04-26 15:08:37 -0700

    100 updated

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 switch = False
-for letter in range(26,0):
+for letter in range(25,-1,-1):
     if switch:
         letter = letter + 65
         switch = False
     else:
         letter = letter + 97
         switch = True
-    print(letter, end='')
+    print(chr(letter), end='')
