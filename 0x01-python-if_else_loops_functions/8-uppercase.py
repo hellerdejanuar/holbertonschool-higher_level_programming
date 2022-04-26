74f859bb41c27ad9d60448b5a4691e06bd1d4a4b
hellerdejanuar <hellerdejanuar@gmail.com>
2022-04-26 14:04:09 -0700

    9 style correced

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
 
-# iterate through letters in string
+    # iterate through letters in string
     for letter in str:
-# if lowercase 
+    # if lowercase
         ascii_ch = ord(letter)
         if ascii_ch >= 97 and ascii_ch <= 122:
             letter = chr(ascii_ch - 32)
