e9f2fb53179aa4364fcee71d5ac0a6937050947e
hellerdejanuar <hellerdejanuar@gmail.com>
2022-04-28 08:23:46 -0700

    1 added

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import calculator_1
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+
+    print("{a} + {b} = {add(a,b)}")
+    print("{a} - {b} = {sub(a,b)}")
+    print("{a} * {b} = {mul(a,b)}")
+    print("{a} / {b} = {div(a,b)}")
