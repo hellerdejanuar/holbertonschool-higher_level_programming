8bafda94f8ba076a376c2fc104b6ef5b838361e8
hellerdejanuar <hellerdejanuar@gmail.com>
2022-04-28 08:25:44 -0700

    1 updated

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
-import calculator_1
+import calculator_1 as calc
 
 if __name__ == "__main__":
     a = 10
     b = 5
 
-    print("{a} + {b} = {add(a,b)}")
-    print("{a} - {b} = {sub(a,b)}")
-    print("{a} * {b} = {mul(a,b)}")
-    print("{a} / {b} = {div(a,b)}")
+    print("{a} + {b} = {calc.add(a,b)}")
+    print("{a} - {b} = {calc.sub(a,b)}")
+    print("{a} * {b} = {calc.mul(a,b)}")
+    print("{a} / {b} = {calc.div(a,b)}")
