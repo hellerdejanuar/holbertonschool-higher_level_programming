b66b23355ee7e51c149fe8cb80410d6afcd236fa
hellerdejanuar <hellerdejanuar@gmail.com>
2022-04-28 13:21:46 -0700

    100 updated

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,9 +4,14 @@ if __name__ == "__main__":
     import calculator_1 as calc
     from sys import argv
     argc = len(argv)
+
+    if argc != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    op = argv[2]
     a = int(argv[1])
     b = int(argv[3])
-    op = argv[2]
 
     if op == '+':
         result = calc.add(a, b)
@@ -21,4 +26,3 @@ if __name__ == "__main__":
         exit(1)
 
     print(f"{a} {op} {b} = {result}")
-    exit(1)
