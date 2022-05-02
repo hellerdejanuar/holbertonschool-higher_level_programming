e110571bec935415ab744489a228f47c6c8b89ff
hellerdejanuar <hellerdejanuar@gmail.com>
2022-05-02 10:46:21 -0700

    5 updated

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -6,9 +6,7 @@ def no_c(my_string):
     new_string = ""
 
     while i < str_len:
-        if (my_string[i] == 'c' or my_string[i] == 'C'):
-            i += 1
-        if i < str_len:
+        if (my_string[i] != 'c' and my_string[i] != 'C'):
             new_string += my_string[i]
         i += 1
     return (new_string)
