36a73bcfcc8950a882e51a4952efd6262cf05e94
hellerdejanuar <hellerdejanuar@gmail.com>
2022-05-02 17:27:30 -0700

    7 updated

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_ar = [tuple_a, tuple_b] 
-    i = 0 # tuple iterator
-    j = 0 # variable iterator
-    result = [0, 0] # array to store the sum
+    tuple_ar = [tuple_a, tuple_b]
+    tp_i = 0  # tuple iterator
+    va_i = 0  # variable iterator
+    result = [0, 0]  # array to store the sum
 
     for tupl in tuple_ar:
-        j = 0 
-        for var in tupl:
-            result[j] += var
-            j += 1
-        i += 1
+        va_i = 0
+
+        if tp_i >= len(result):
+            break
+
+        for elem in tupl:
+            result[j] += elem
+            va_i += 1
+        tp_i += 1
 
     result_tpl = (result[0], result[1])
 
