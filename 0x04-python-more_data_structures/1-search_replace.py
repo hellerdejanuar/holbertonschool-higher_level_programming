2314047a601c70d4b2df1938c941877e699f2a88
hellerdejanuar <hellerdejanuar@gmail.com>
2022-05-04 12:18:26 -0700

    1 upd

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
+    if not my_list:
+        return None
+
     new_list = my_list.copy()
+
     for elem in new_list:
         if elem == replace:
             elem = replace
