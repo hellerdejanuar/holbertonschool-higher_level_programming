483621a73354f9753072fa590378259c434cdb52
hellerdejanuar <hellerdejanuar@gmail.com>
2022-05-16 14:28:00 -0700

    7 add

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as excp:
+        print("Exception: {}".format(excp), file=sys.stderr)
+        return False
