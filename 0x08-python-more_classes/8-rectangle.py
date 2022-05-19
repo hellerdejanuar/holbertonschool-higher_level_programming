9a16f763f5521be89ad65127ea2cca92f0defd92
hellerdejanuar <hellerdejanuar@gmail.com>
2022-05-19 10:53:24 -0700

    8 add

---
+++ 0x08-python-more_classes/8-rectangle.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python3
+""" Class Rectangle """
+
+
+class Rectangle:
+    """Rectangle"""
+
+    number_of_instances = 0
+    print_symbol = "#"
+    
+    def bigger_or_equal(rect_1, rect_2):
+        """return biggest rectangle based on area """
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        
+        if rect_1.area() < rect_2.area():
+            return rect_2
+        else:
+            return rect_1
+
+    def __init__(self, width=0, height=0):
+        """initialize rectangle"""
+        self.height = height
+        self.width = width
+        Rectangle.number_of_instances += 1
+
+    def __repr__(self):
+        """representation of rectangle to recreate"""
+        return (f"{self.__class__.__name__}({self.width}, {self.height})")
+
+    def __str__(self):
+        """print rectangle with symbol """
+        if self.width == 0 or self.height == 0:
+            return ""
+        else:
+            return (f"{str(self.print_symbol)*self.width}\n"
+                    * self.height).strip('\n')
+
+    def __del__(self):
+        """delete rectangle routine"""
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
+
+    def area(self):
+        """area: width * height"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """perimeter: (width*2) + (height*2)"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return self.__width*2 + self.__height*2
+
+    @property
+    def height(self):
+        """height getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height setter"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    @property
+    def width(self):
+        """width getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width setter"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
