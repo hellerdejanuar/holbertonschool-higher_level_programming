4e5dd0fcf0b55ae8b9c6d9e6ce2d79fb2d3070eb
hellerdejanuar <hellerdejanuar@gmail.com>
2022-05-31 15:28:08 -0700

    1 upd

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,8 +4,9 @@
 
 def write_file(filename="", text=""):
     """ writes a string to a file """
+    
     if not filename:
         raise Exception('No file')
 
     with open(filename, w, encoding="utf-8") as f:
-       f.write()
+       f.write(text)
