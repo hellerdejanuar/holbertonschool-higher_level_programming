9d77fe3f188c345504ffbbd8fad3d79a194a6e02
hellerdejanuar <hellerdejanuar@gmail.com>
2022-05-31 16:00:35 -0700

    4 upd

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python3
 """ deserializing """
 import json
@@ -6,4 +7,4 @@ import json
 def from_json_string(my_str):
     """ converts string to JSON representation """
 
-    return(json.loads(my_str))
+    return(json.load(my_str))
