5b3009c7f8441fac90f9da135085c180615dfd37
hellerdejanuar <hellerdejanuar@gmail.com>
2022-05-30 17:35:02 -0700

    3 upd

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+""" is kind module """
+
 
 def is_kind_of_class(obj, a_class):
+    """ checks if is class or sublcass """
+
     if isinstance(obj, a_class):
         return True
-    else: 
+    else:
         return False
