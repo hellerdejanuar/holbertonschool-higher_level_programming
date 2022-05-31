ada11cac5e49851eac9e19cf96af87cb94799158
hellerdejanuar <hellerdejanuar@gmail.com>
2022-05-31 15:54:24 -0700

    3 add

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -4,9 +4,4 @@
 def to_json_string(my_obj):
     """ converts JSON representation of an object """
 
-    if not my_obj:
-        raise Exception('No object')
-
-    with open(filename, 'a', encoding="utf-8") as f:
-        ch_cn = f.write(text)
-    return ch_cn
+    return(json.dumps(my_obj))
