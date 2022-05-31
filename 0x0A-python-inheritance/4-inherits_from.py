a4250fc38a163d2c3db9084861e4fce88b66741a
hellerdejanuar <hellerdejanuar@gmail.com>
2022-05-30 17:36:22 -0700

    5 upd

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+""" inherits module """
+
 
 def inherits_from(obj, a_class):
+    """ checks if object inherits attributes from a class """
+
     if isinstance(obj, a_class) and (type(obj) != a_class):
         return True
-    else: 
+    else:
         return False
