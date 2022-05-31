1594671c44873c022b03b290775f886bf735d24f
hellerdejanuar <hellerdejanuar@gmail.com>
2022-05-31 15:27:15 -0700

    1 add

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" write file module """
+
+
+def write_file(filename="", text=""):
+    """ writes a string to a file """
+    if not filename:
+        raise Exception('No file')
+
+    with open(filename, w, encoding="utf-8") as f:
+       f.write()
