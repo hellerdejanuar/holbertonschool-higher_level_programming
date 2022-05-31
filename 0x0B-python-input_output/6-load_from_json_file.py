b049dec547d2d516743086d4e78c3e6c45affde1
hellerdejanuar <hellerdejanuar@gmail.com>
2022-05-31 16:21:57 -0700

    6 add

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" deserializing """
+import json
+
+def load_from_json_file(filename):
+    """ load JSON representation from filename """
+
+    with open(filename, 'r', encoding="utf-8") as f:
+        json.load(my_obj, f)
