e32e0fe434dd26bb3de9ecec40e2fa4009bdbad8
hellerdejanuar <hellerdejanuar@gmail.com>
2022-05-31 19:52:02 -0700

    11 add

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" student module """
+
+
+class Student:
+    """ defines a student """
+
+    def __init__(self, first_name, last_name, age):
+        """ init student """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ returns a dict of object attributes """
+        if isinstance(attrs, list):
+            if all(isinstance(elem, str) for elem in attrs):
+                dic = {}
+                for att in attrs:
+                    if att in self.__dict__.keys():
+                        dic[att] = self.__dict__[att]
+                return dic
+        else:
+            return(self.__dict__)
+
+    def reload_from_json(self, json):
+        """ reload from json dict """
+        for att in json:
+            self.__dict__.update({att: json[att]})
