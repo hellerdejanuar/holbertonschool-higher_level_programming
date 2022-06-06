a500e7fdef15783a85447086c3f04e2cdc3f6720
hellerdejanuar <devanheller@gmail.com>
2022-06-06 14:08:44 -0700

    10 add

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 """ square module """
+from models.base import Base
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
     """ square class """
+
+    classname = "Square"
+
     def __init__(self, size, x=0, y=0, id=None):
         """ initializes square """
         super().__init__(size, size, x, y, id)
@@ -12,3 +16,13 @@ class Square(Rectangle):
     def __str__(self):
         return (f"[{self.classname}] ({self.id})"
                 f" {self.__x}/{self.__y} - {self.__height}")
+    @property
+    def size(self):
+        """ Size Getter """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ Size Setter """
+        self.width = value
+        self.height = value
