d43996b5bdb10dd643fa8b2bc1377287d7a8b741
hellerdejanuar <devanheller@gmail.com>
2022-06-06 16:08:28 -0700

    15 add

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -38,3 +38,10 @@ class Base:
             elif cond == 'positiveOrZero':
                 if value < 0:
                     raise ValueError(f"{name} must be >= 0")
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ JSON string representation of list_dictionaries """
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        return json.dumps(list_dictionaries)
