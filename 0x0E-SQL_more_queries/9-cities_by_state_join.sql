50d2baae659583b9080c937835d6e5a97fb7f7cf
hellerdejanuar <hellerdejanuar@gmail.com>
2022-07-06 13:44:55 -0700

    9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,4 @@
 -- lists all the cities of California that can be found in the database hbtn_0d_usa
 SELECT cities.id, cities.name, states.name
-FROM cities, states
-WHERE cities.state_id = states.id
+FROM cities, states WHERE cities.state_id = states.id
 ORDER BY id ASC;
