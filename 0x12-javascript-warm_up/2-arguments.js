ebc106202edd5b1ce5e98f199ac7284e7ca60856
hellerdejanuar <hellerdejanuar@gmail.com>
2022-08-01 12:05:44 -0700

    2 added

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,11 +2,9 @@
 
 const process = require('process');
 if (process.argv.length === 2) {
-	console.log('No argument');
-}
-else if (process.argv.length === 3) {
-	console.log('Argument found');
-}
-else {
-	console.log('Arguments found');
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
