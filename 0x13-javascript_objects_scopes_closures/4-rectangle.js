94571fcfa54f0b18235d871920108742c7251e3e
hellerdejanuar <hellerdejanuar@gmail.com>
2022-08-02 09:53:17 -0700

    4 add

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const chr = 'X';
+    let line = '';
+
+    for (let j = 0; j < this.width; j++) {
+      line = line + chr;
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(line);
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
