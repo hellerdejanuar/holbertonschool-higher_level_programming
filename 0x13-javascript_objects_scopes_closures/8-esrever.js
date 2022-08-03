850b576bfa49d0b920c35c6b374b06cc92fd73b1
hellerdejanuar <hellerdejanuar@gmail.com>
2022-08-03 09:56:30 -0700

    8 fix

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  return (list.reverse());
+  const newlist = [];
+  let i;
+  for (i = list.length - 1; i >= 0; i--) {
+    newlist.push(list[i]);
+  }
+  return (newlist);
 };
