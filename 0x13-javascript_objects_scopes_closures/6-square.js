043f2ab800c8d762674b7e9d7857b7dded074a27
hellerdejanuar <hellerdejanuar@gmail.com>
2022-08-02 19:16:39 -0700

    6 add

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint(c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+
+    let line = '';
+
+    for (let j = 0; j < this.width; j++) {
+      line = line + c;
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(line);
+    }
+    }
+  }
+};
