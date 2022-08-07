b457d39cca791d9b6199434a52abcae39c63169e
hellerdejanuar <hellerdejanuar@gmail.com>
2022-08-07 12:07:38 -0700

    7 add

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Table fetcher """
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import select
+from sys import argv
+
+USER_IN = argv[1]
+PASSWD_IN = argv[2]
+DB_IN = argv[3]
+HOST_IN = 'localhost'
+
+engine = create_engine(f'mysql+mysqldb://{USER_IN}:{PASSWD_IN}@{HOST_IN}/{DB_IN}')
+session = Session(bind=engine)
+
+query = session.query(State.name, State.id).all()
+for name, state_id in query:
+    print(f'{state_id}: {name}')
+
+session.close()
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" contains the class definition of a State and an instance Base = declarative_base() """
+""" contains the class definition of a State
+and an instance Base = declarative_base() """
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
@@ -9,6 +10,6 @@ Base = declarative_base()
 class State(Base):
     """ states class """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, 
+    id = Column(Integer, primary_key=True,
                autoincrement=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
