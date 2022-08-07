0234411a1c51e643ef0fa528c754090a0da72267
hellerdejanuar <hellerdejanuar@gmail.com>
2022-08-07 15:24:51 -0700

    10 add

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Fetches the first state ordered by ID """
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import select
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    USER_IN = argv[1]
+    PASSWD_IN = argv[2]
+    DB_IN = argv[3]
+    HOST_IN = 'localhost'
+    STATE_IN = ' '.join(argv[4:])
+
+    engine = create_engine(f'mysql+mysqldb://{USER_IN}:\
+                       {PASSWD_IN}@{HOST_IN}/{DB_IN}')
+    session = Session(bind=engine)
+
+    query = session.query(State.name, State.id).\
+        filter(State.name.like(STATE_IN))
+    if query:
+        for state in query:
+            print(f'{state.id}')
+    else:
+        print('Not found')
+
+    session.close()
