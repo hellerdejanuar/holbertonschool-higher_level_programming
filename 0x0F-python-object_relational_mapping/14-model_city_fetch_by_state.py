97a109303a9987af93bd9f23cb53280274015982
hellerdejanuar <hellerdejanuar@gmail.com>
2022-08-07 19:31:15 -0700

    14 fix

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Cities fetcher """
 
+
 from model_state import Base, State
 from model_city import City
 from sqlalchemy import create_engine
@@ -8,20 +9,20 @@ from sqlalchemy.orm import Session
 from sqlalchemy import Column, Integer, String
 from sys import argv
 
-USER_IN = argv[1]
-PASSWD_IN = argv[2]
-DB_IN = argv[3]
-HOST_IN = 'localhost'
+if __name__ == '__main__':
+    USER_IN = argv[1]
+    PASSWD_IN = argv[2]
+    DB_IN = argv[3]
+    HOST_IN = 'localhost'
 
 
-engine = create_engine(f'mysql+mysqldb://{USER_IN}:\
+    engine = create_engine(f'mysql+mysqldb://{USER_IN}:\
                         {PASSWD_IN}@{HOST_IN}/{DB_IN}')
 
-with Session(bind=engine) as session:
-    city_q = (session.query(City, State)
-              .join(State, State.id == City.state_id)
-              .order_by(City.id)
-              .all())
+    with Session(bind=engine) as session:
+        city_q = (session.query(City, State)
+                  .join(State, State.id == City.state_id)
+                  .all())
 
-    for city, state in city_q:
-        print(f'{state.name}: ({city.id}) {city.name}')
+        for city, state in city_q:
+            print(f'{state.name}: ({city.id}) {city.name}')
