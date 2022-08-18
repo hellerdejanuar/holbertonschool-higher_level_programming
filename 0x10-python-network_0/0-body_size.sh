e8c10c7e872d6128ca042a9944d13a492f641490
hellerdejanuar <hellerdejanuar@gmail.com>
2022-08-18 11:34:44 -0700

    0 fix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # parses the specified parameter from the request header. needs the ip as argument
 
-curl -s -I /dev/null "$1" | grep  "Content-Length: " | sed 's/^.*: //'
+curl -sI "$1" | grep  "Content-Length: " | sed 's/^.*: //'
