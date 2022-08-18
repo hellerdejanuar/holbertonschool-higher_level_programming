b040d6c9fce9544f53cf2f662cbd848e75a56df5
hellerdejanuar <hellerdejanuar@gmail.com>
2022-08-18 11:13:57 -0700

    0 add

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# parses the specified parameter from the request header. needs the ip as argument
+if [[ ! $1 ]]; then
+echo 'IP required'
+else
+ip=$1
+param='Content-Length' # set desired param name without (:)
+curl -s -I /dev/null "$ip" | grep  "$param: " | sed 's/^.*: //'
+fi
