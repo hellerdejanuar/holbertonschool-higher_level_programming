02e7557f2c7dee9716ef74266b9a66b219683e7f
hellerdejanuar <hellerdejanuar@gmail.com>
2022-08-19 12:07:08 -0700

    5 add

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # parses the specified parameter from the request header. needs the ip as argument
-curl -s -X POST -H "email:test@gmail.com" -H "subject:test@gmail.com" "$1"
+curl -sd "email=test@gmail.com&subject=I will always be here for PLD"
