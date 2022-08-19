24cb9892f00917fc3c65fe73e78a82d4a0514077
hellerdejanuar <hellerdejanuar@gmail.com>
2022-08-19 13:55:48 -0700

    2 add

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+this python script takes in a URL, sends a request to the URL and displays
+the value of the X-Request-Id variable found in the header of the response.
+"""
+
+from sys import argv
+from urllib import request, parse
+
+if __name__ == "__main__":
+    data = parse.urlencode({'email': argv[2]})
+                            # 'method':'POST'})
+    data = data.encode('ascii')
+
+    req = request.Request(argv[1], data)
+    resp = request.urlopen(req)
+    html = resp.read().decode('utf-8')
+    print(html)
