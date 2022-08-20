c4294d9e4d5a484239935a3f3f11a22fbe4cc5d3
hellerdejanuar <hellerdejanuar@gmail.com>
2022-08-19 18:04:20 -0700

    3 add

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+takes in a URL, sends a request to the URL and displays the body of the response
+"""
+
+from sys import argv
+from urllib import request, parse
+import urllib.error
+
+if __name__ == "__main__":
+    try:
+        with request.urlopen(argv[1]) as resp:
+            html = resp.read().decode('utf-8')
+            print(html)
+    except urllib.error.HTTPError as error:
+        print(f'Error code: {error.code}')
+        raise
