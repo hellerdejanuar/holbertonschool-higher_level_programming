2fcef07933820f3e52f13c9b0f4ca3fadac84797
hellerdejanuar <hellerdejanuar@gmail.com>
2022-08-22 08:56:02 -0700

    8 style fix

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.
+takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
 """
 
 
@@ -10,13 +11,13 @@ if __name__ == "__main__":
 
     url = 'http://0.0.0.0:5000/search_user'
     data = {}
-    
+
     # defining data variables
     if len(argv) > 1:
         data['q'] = sys.argv[1]
     else:
         data['q'] = ''
-    
+
     # request
     resp = requests.post(url, data=data)
 
