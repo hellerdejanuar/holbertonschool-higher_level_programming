24776682b7798a64d723bff1261174b902600198
hellerdejanuar <hellerdejanuar@gmail.com>
2022-08-22 08:38:07 -0700

    7 add

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+takes in a URL, sends a request to the URL0 (with requests)
+and displays the body of the response
+"""
+
+if __name__ == "__main__":
+    from sys import argv
+    import requests
+
+    resp = requests.get(argv[1])
+    print(resp.text)
+
+    if resp.status_code >= 400:
+        print("Error code: {}".format(resp.status_code))
