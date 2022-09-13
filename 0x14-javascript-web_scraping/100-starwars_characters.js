4364013941aa0e9fee843e08e6017e6cf9ee4af6
hellerdejanuar <hellerdejanuar@gmail.com>
2022-09-13 21:46:37 +0000

    100 add

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,18 +1,17 @@
 #!/usr/bin/node
 
 const axios = require('axios').default;
-const url = 'https://swapi-api.hbtn.io/api/films/'+ process.argv[2];
-let cn = 0;
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
 axios.get(url)
   .then(function (response) {
     // handle success
-    characters_list = response.data.characters;
-    for (let i in characters_list) {
-      axios.get(characters_list[i])
+    const charactersList = response.data.characters;
+    for (const i in characters_list) {
+      axios.get(charactersList[i])
         .then(function (character) {
           console.log(character.data.name);
-      });
+        });
     }
   })
   .catch(function (error) {
