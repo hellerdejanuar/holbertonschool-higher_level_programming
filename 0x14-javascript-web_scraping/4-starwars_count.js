fd9367c79a5040b8a7d114abebc9baa9b7697d8a
hellerdejanuar <hellerdejanuar@gmail.com>
2022-09-13 15:16:17 +0000

    4 fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,14 +7,15 @@ let cn = 0;
 axios.get(url)
   .then(function (response) {
     // handle success
-    const movies = response.data.results
-    for (i in movies) {
-
-      if (movies[i].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
-        cn++;
+    const movies = response.data.results;
+    for (const i in movies) {
+      for (const j in movies[i].characters) {
+        if (movies[i].characters[j].includes('/18/')) {
+          cn++;
+        }
       }
     }
-    console.log(cn)
+    console.log(cn);
   })
   .catch(function (error) {
     // handle error
