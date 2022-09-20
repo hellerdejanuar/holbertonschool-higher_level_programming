dd8ab3e381ccb28e6f47313962c8887ec7661d01
Mauricio Heller <73609560+hellerdejanuar@users.noreply.github.com>
2022-09-20 09:57:35 -0300

    Update 8-script.js

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,5 +1,5 @@
 $.get("https://swapi-api.hbtn.io/api/people/5/?format=json", function (req) {
-  req.forEach(element => { 
+  $.each(req.results, function (element => { 
     $('UL#list_movies').append("<li>" + element.name + "</li>");
     console.log(element.name);
   });
